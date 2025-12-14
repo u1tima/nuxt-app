3d08387cf097e00c5e1b1c4d5f49a9a00dcd9f5a
Vitali <vit-sema@yandex.ru>
2025-12-14 12:25:30 +0300

    add a table of content

---
+++ app/router.options.ts
@@ -0,0 +1,24 @@
+// https://router.vuejs.org/api/interfaces/routeroptions.html
+export default {
+	scrollBehavior(to: { hash: any }, from: any, savedPosition: any) {
+		// `to` and `from` are both route locations
+		// `savedPosition` can be null if there isn't one
+		if (savedPosition) {
+			return savedPosition
+		}
+
+		if (to.hash) {
+			return {
+				el: to.hash,
+				behavior: 'smooth',
+				top: 30
+			}
+		}
+
+		return { 
+			left: 0, 
+			top: 0, 
+			behavior: 'smooth' 
+		}
+	}
+}
