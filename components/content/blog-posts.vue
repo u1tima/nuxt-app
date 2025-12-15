74e638197b8ce87346e9f93367d2b594202de282
Vitali <vit-sema@yandex.ru>
2025-12-15 14:18:22 +0300

    add content reusability

--- components/content/blog-posts.vue
+++ components/content/blog-posts.vue
@@ -32,25 +32,27 @@
 </script>
 
 <template>
-	<section class="not-prose font-mono">
-		<div class="column text-gray-400 text-sm">
-			<div>date</div>
-			<div>title</div>
-		</div>
+	<slot :posts="posts">
+		<section class="not-prose font-mono">
+			<div class="column text-gray-400 text-sm">
+				<div>date</div>
+				<div>title</div>
+			</div>
 
-		<ul>
-			<li v-for="post in posts"
-				:key="post._path">
-				<NuxtLink :to="post._path"
-						  class="column hover:bg-gray-100 dark:hover:bg-gray-800">
-					<div
-						 :class="{ 'text-white dark:text-gray-900': !post.displayYear, 'text-gray-400 dark:text-gray-500': post.displayYear }">
-						{{ post.year }}</div>
-					<div>{{ post.title }}</div>
-				</NuxtLink>
-			</li>
-		</ul>
-	</section>
+			<ul>
+				<li v-for="post in posts"
+					:key="post._path">
+					<NuxtLink :to="post._path"
+							  class="column hover:bg-gray-100 dark:hover:bg-gray-800">
+						<div
+							 :class="{ 'text-white dark:text-gray-900': !post.displayYear, 'text-gray-400 dark:text-gray-500': post.displayYear }">
+							{{ post.year }}</div>
+						<div>{{ post.title }}</div>
+					</NuxtLink>
+				</li>
+			</ul>
+		</section>
+	</slot>
 </template>
 
 <style scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,5 @@
-<script setup>
-	useHead({
-		title: 'My App',
-		meta: [
-			{ name: 'description', content: 'My amazing site.' }
-		],
-		bodyAttrs: {
-			class: 'test'
-		},
-		script: [{ innerHTML: 'console.log(\'Hello world\')' }]
-	})
-
-	useSeoMeta({
-		title: 'My Amazing Site',
-		ogTitle: 'My Amazing Site',
-		description: 'This is my amazing site, let me tell you all about it.',
-		ogDescription: 'This is my amazing site, let me tell you all about it.',
-		ogImage: 'https://example.com/image.png',
-		twitterCard: 'summary_large_image',
-	})
-
-
-</script>
-
 <template>
-	<div>Hello</div>
+	<article class="prose dark:prose-invert max-w-none">
+		<ContentDoc path="/" />
+	</article>
 </template>
